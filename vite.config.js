a40dd633878e25aeb71b3a8947bb98870e0e3416
Nicolás Foti <nicolasdfoti@gmail.com>
2025-06-11 12:02:49 -0300

    Added pages to dist directory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path"
 
 export default defineConfig({
-  base: "./",
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        collaborate: resolve(__dirname, "src/collaborate/index.html"),
+        favorite_spots: resolve(__dirname, "src/favorite-spots/index.html")
+      }
+    }
+  }
+})
