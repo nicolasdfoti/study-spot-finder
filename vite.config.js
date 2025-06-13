994647500053404740931a73c677e862c032a969
Nicolás Foti <nicolasdfoti@gmail.com>
2025-06-12 22:13:50 -0300

    Added pages to Vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         collaborate: resolve(__dirname, "src/collaborate/index.html"),
-        favorite_spots: resolve(__dirname, "src/favorite-spots/index.html")
+        favorite_spots: resolve(__dirname, "src/favorite-spots/index.html"),
+        coffee_shops: resolve(__dirname, "src/categories/coffee-shops.html"),
+        libraries: resolve(__dirname, "src/categories/libraries.html"),
+        parks: resolve(__dirname, "src/categories/parks.html"),
       }
     }
   }
