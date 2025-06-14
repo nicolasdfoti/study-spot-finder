e9bc299075dd64b5c82b8667fa4996a7c02116cb
Nicolás Foti <nicolasdfoti@gmail.com>
2025-06-14 20:15:42 -0300

    Added folders

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         collaborate: resolve(__dirname, "src/collaborate/index.html"),
         favorite_spots: resolve(__dirname, "src/favorite-spots/index.html"),
-        coffee_shops: resolve(__dirname, "src/coffee-shops/index.html"),
-        libraries: resolve(__dirname, "src/libraries/index.html"),
-        parks: resolve(__dirname, "src/parks/index.html")
+        coffee_shops: resolve(__dirname, "src/categories/coffee-shops/index.html"),
+        libraries: resolve(__dirname, "src/categories/libraries/index.html"),
+        parks: resolve(__dirname, "src/categories/parks/index.html")
       }
     }
   }
