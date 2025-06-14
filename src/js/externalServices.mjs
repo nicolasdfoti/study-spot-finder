2ccf8faa9cdafe81a9020c375a53e20ab458eb4b
Nicolás Foti <nicolasdfoti@gmail.com>
2025-06-14 19:36:49 -0300

    Added base to External Services

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,16 +1,18 @@
 // EXTERNAL SERVICES: External Data (and from JSON)
 
+const base = import.meta.env.BASE_URL;
+
 export async function getCoffeeSpots() {
-  const res = await fetch("../../public/data/coffee-shops.json");
+  const res = await fetch(`${base}data/coffee-shops.json`);
   return await res.json();
 }
 
 export async function getLibrarySpots() {
-  const res = await fetch("../../public/data/libraries.json");
+  const res = await fetch(`${base}data/libraries.json`);
   return await res.json();
 }
 
 export async function getParkSpots() {
-  const res = await fetch("../../public/data/parks.json");
+  const res = await fetch(`${base}data/parks.json`);
   return await res.json();
 }
